b0235752a869dd52cc5ad12b7633dbb9db16527c
Aleksandr Slapoguzov <aleksandr-slap@rambler.ru>
2021-03-12 16:14:00 +0300

    Lesson - 3

--- Lesson-3/train.rb
+++ Lesson-3/train.rb
@@ -1,13 +1,12 @@
 class Train
 
-  attr_accessor :speed, :route, :current_station
-  attr_reader :amount_wagons, :type, :name
+  attr_accessor :speed, :current_station
+  attr_reader :amount_wagons, :type
 
   def initialize(number, type, amount_wagons)
     @number = number
     @type = type
     @amount_wagons = amount_wagons
-    @route = route
     @speed = 0
   end
 
@@ -25,30 +24,36 @@ class Train
 
   def get_route(route)
     @route = route
-    @current_station = route.first_station
-  end
-
-  def move_back_station
-    if current_station == @route.first_station
-      puts "It'sy first_station"
-    else
-      @current_station = @route.all_stations[index_current_station - 1]
-    end
+    @current_station = @route.first_station
+    @current_station.park_train(self)
   end
 
   def move_next_station
     if current_station == @route.last_station
       puts "It's last_station"
     else
-     @current_station = @route.all_stations[index_current_station + 1]
+     @current_station.go_train(self)
+     @current_station = @route.all_station[index_current_station + 1]
     end
+     @current_station.park_train(self)
   end
 
-  def show_next_station
-    if current_station == route.last_station
+  def move_back_station
+    if current_station == @route.first_station
+      puts "It'sy first_station"
+    else
+      @current_station.go_train(self)
+      @current_station = @route.all_station[index_current_station - 1]
+    end
+    @current_station.park_train(self)
+  end
+
+
+  def next_station
+    if current_station == @route.last_station
       puts "It's last_station"
     else  
-      @route.all_stations[index_current_station + 1]
+      @route.all_station[index_current_station + 1]
     end  
   end 
 
@@ -57,15 +62,15 @@ class Train
   end 
 
   def show_back_station
-    if current_station == route.first_station
+    if current_station == @route.first_station
       puts "It's first station"
     else  
-      @route.all_stations[index_current_station - 1]
+      @route.all_station[index_current_station - 1]
     end  
   end
 
   def index_current_station
-    @route.all_stations.index(@current_station)
+    @route.all_station.index(@current_station)
   end
 end
 
@@ -73,45 +78,41 @@ require './route.rb'
 require './station.rb'
 
 train1 = Train.new("Red Arrow", "passenger", 12)
-# train2 = Train.new("Cargo345", "cargo", 56)
-# train3 = Train.new("Cargo123", "cargo", 91)
-# train4 = Train.new("Star", "passenger", 56)
+train2 = Train.new("Cargo345", "cargo", 56)
+train3 = Train.new("Cargo123", "cargo", 91)
+train4 = Train.new("Star", "passenger", 56)
 
 
 
-mga = Station.new("Mga")
 volhov = Station.new("Volhov")
+mga = Station.new("Mga")
+giharevo = Station.new("Giharevo")
 spb = Station.new("SPB")
 
 rout = Route.new(volhov, spb)
 rout.add_station(mga)
-rout.all_station
+rout.add_station(giharevo)
+# p rout.all_station
 
 train1.get_route(rout)
-
-# p train1.show_current_station
-# p train1.show_back_station
-# p train1.show_next_station
+puts volhov.trains.size
 
 
-# mga.park_train(train1)
-# mga.park_train(train2)
-# mga.park_train(train3)
-# mga.park_train(train4)
+train1.move_next_station
+volhov.trains.size
 
-# mga.go_train(train1)
-# mga.go_train(train3)
-# mga.go_train(train2)
-# p train1.move_next_station
+mga.show_type_train
+train1.move_next_station
+train1.move_next_station
 
-# p train1.move_next_station
+spb.show_type_train
+train1.move_back_station
+giharevo.show_type_train
+train1.move_back_station
+mga.show_type_train
+train1.move_back_station
+giharevo.show_type_train
 
-# p train1.move_next_station
-# p train1.move_back_station
-# p train1.move_back_station
-
-# p train1.show_back_station
-# p train1.show_current_station
-# p train1.show_next_station
-
-p rout.all_station
+p train1.current_station
+p train1.next_station
+puts train1.show_back_station
