127c70fea1a8596a181c96bbcac06ed7ab4e852f
Aleksandr Slapoguzov <aleksandr-slap@rambler.ru>
2021-03-16 23:35:34 +0300

    Leson - 4(test)

---
+++ Lesson-4/route.rb
@@ -0,0 +1,23 @@
+class Route
+  attr_reader :first_station, :last_station
+
+  def initialize (first_station,
+                  last_station,
+                  intermediate_stations = [])
+    @first_station = first_station
+    @last_station = last_station
+    @intermediate_stations = intermediate_stations
+  end
+
+  def add_station(station)
+    @intermediate_stations << station
+  end
+
+  def delete_station(station)
+    @intermediate_stations.delete(station)
+  end
+
+  def all_station
+    [@first_station] + @intermediate_stations + [@last_station]
+  end
+end
---
+++ Lesson-4/vagon_c.rb
@@ -0,0 +1,9 @@
+class CargoVagon
+
+	attr_reader :type_vagon
+
+	def initialize(number)
+	  @number = number
+	  @type_vagon = "cargo"
+  end
+end
